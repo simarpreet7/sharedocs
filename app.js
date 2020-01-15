9eab57404f5251ccb18e26ffb7fe6e429b5b2882
simarpreetsingh7 <samsingh7039.dtu@gmail.com>
2020-01-15 08:13:28 +0530

    login and word chnaged

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var express=require("express")
-var app= express()
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    User                    = require("./models/user"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose")
+    
+var app = express();
 path = require('path'),
-app.set("view engine", "ejs")
-var bodyParser = require("body-parser"); 
-var mongoose=require("mongoose")
-mongoose.connect("mongodb://localhost:27017/share",{useNewUrlParser:true})
-
-app.use(bodyParser.urlencoded({ extended: true })); 
-
 app.use('/public', express.static('public'));
+
+
+
+mongoose.connect("mongodb://localhost/share");
+
 
 
 
@@ -20,15 +25,39 @@ const share=mongoose.model("share",shareschema)
 
 
 
- 
-  app.get('/', function(req, res) {
-    res.render('index.ejs');
 
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(require("express-session")({
+    secret:"sam says wow",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.set('view engine','ejs');
+//Then, tell express to use Passport:
+app.use(passport.initialize());
+app.use(passport.session());
+// 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/secret",isLoggedIn, function(req, res){
+    res.render("secret");
 });
 
 
-  app.post('/example', (req, res) => {
-    res.send(req.body.fname);
+
+
+
+app.post('/save', (req, res) => {
+    res.send("data saved succesful");
     
   var document1 = new share({name:req.body.fname})
   document1.save();
@@ -36,7 +65,57 @@ const share=mongoose.model("share",shareschema)
 
   });
 
- app.listen(3000)
+// Auth Routes
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//handling user sign up
+app.post("/register", function(req, res){
+User.register(new User({username:req.body.username}),req.body.password, function(err, user){
+       if(err){
+            console.log(err);
+            return res.render('register');
+        } //user stragety
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/login"); //once the user sign up
+       }); 
+    });
+});
+
+// Login Routes
+
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+// middleware
+app.post("/login", passport.authenticate("local",{
+    successRedirect:"/secret",
+    failureRedirect:"/login"
+}),function(req, res){
+    res.send("User is "+ req.user.id);
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 
+
+
+
+
+
+
+app.listen(3000);
