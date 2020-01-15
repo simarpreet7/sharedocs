6a9e059dd0e25a2c38990f08d4a8eb9bb6b27160
simarpreetsingh7 <samsingh7039.dtu@gmail.com>
2020-01-15 13:44:47 +0530

    final login commit

--- app.js
+++ app.js
@@ -1,123 +1,124 @@
-var express                 = require("express"),
-    mongoose                = require("mongoose"),
-    passport                = require("passport"),
-    bodyParser              = require("body-parser"),
-    User                    = require("./models/user"),
-    LocalStrategy           = require("passport-local"),
-    passportLocalMongoose   = require("passport-local-mongoose")
-    
+var express = require("express"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  User = require("./models/user"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose");
+ 
 var app = express();
-path = require('path'),
-app.use('/public', express.static('public'));
-
-
+path = require("path");
+app.use("/public", express.static("public"));
 
 mongoose.connect("mongodb://localhost/share");
 
+var Schema = mongoose.Schema;
 
+var shareschema = new Schema({
+  name: String,
+  created_by: String
+});
 
+var _document = mongoose.model("document", shareschema);
 
-const Schema=mongoose.Schema;
-
-const shareschema=new Schema({name:String,created_by:String});
-
-const _document=mongoose.model("document",shareschema)
-
-
-
-
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(require("express-session")({
-    secret:"sam says wow",
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(
+  require("express-session")({
+    secret: "sam says wow",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 //Then, tell express to use Passport:
 app.use(passport.initialize());
 app.use(passport.session());
-// 
+//
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.get("/",function(req,res){
-    res.render("home");
-});
-
-app.get("/word",isLoggedIn, function(req, res){
-    res.render("word");
+app.get("/", function(req, res) {
+  res.render("login");
 });
 
 
+app.get("/word/:id", isLoggedIn, function(req, res) {
+  // req.params= { id:req.user.id}
+  res.render("word");
+});
 
-
-
-app.post('/save', (req, res) => {
-    res.send("data saved succesful");
-    
-  var new_document = new _document({name:req.body.fname,created_by:req.body.username})
-  new_document.save();
-
-
+app.post("/save", function(req, res) {
+  res.send("data saved succesful");
+  //two files require to access username and fname
+  var new_document = new _document({
+    name: req.body.fname,
+    created_by: req.body.username
   });
+  new_document.save();
+});
 
 // Auth Routes
 
-app.get("/signup", function(req, res){
-    res.render("signup");
+app.get("/signup", function(req, res) {
+  res.render("signup");
 });
+
 //handling user sign up
-app.post("/signup", function(req, res){
-User.register(new User({username:req.body.username}),req.body.password, function(err, user){
-       if(err){
-            console.log(err);
-            return res.render('signup');
-        } //user stragety
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/login"); //once the user sign up
-       }); 
-    });
+app.post("/signup", function(req, res) {
+  
+  User.register(
+    new User({
+      username: req.body.username
+    }),
+    req.body.password,
+    function(err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("signup");
+      } //user stragety
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/login"); //once the user sign up
+      });
+    }
+  );
 });
 
 // Login Routes
 
-app.get("/login", function(req, res){
-    res.render("login");
-})
+app.get("/login", function(req, res) {
+  res.render("login");
+});
 
 // middleware
-app.post("/login", passport.authenticate("local",{
-    successRedirect:"/word",
-    failureRedirect:"/login"
-}),function(req, res){
-    res.send("User is "+ req.user.id);
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    // successRedirect: "/word",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {
+    // res.send("User is " + req.user.id);
+    res.redirect("/word/"+req.body.username)
+  }
+);
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
-app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
-});
-
-
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    //user does not exist
-    res.redirect("/login");
-    
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  //user does not exist
+  res.redirect("/login");
 }
-
-
-
-
-
-
-
-
 
 app.listen(3000);
