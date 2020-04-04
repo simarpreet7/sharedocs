4aa70343e58d2c9c164e4e5348f982ba0598a1ad
simarpreetsingh7 <samsingh7039.dtu@gmail.com>
2020-04-04 15:44:58 +0530

    autosaving done

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var express = require('express');
 
 realtimeEditor.onSave(function (data) {	
   console.log('realtimeEditor.onSave: ', data);
-  //saving these changes in db with corresponding user and document autosave
+  
   //document name after space error
   ///tables layout proper
   ///z-index of toolbar
